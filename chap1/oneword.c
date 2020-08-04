063f0d8051c1a25d6628d214eba7b89f1eec8732
James Boxall <jimmyboxall@gmail.com>
2020-08-04 12:05:01 +1000

    Finished 1-11 and 1-12. Added oneword.c

---
+++ chap1/oneword.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#define IN  1    /* inside a group of whitespace characters */
+#define OUT 0    /* outside of whitespace */
+
+/* copy input to output, replace sets of blanks with one blank */
+
+int main()
+{
+	int c, state;
+	state = OUT;
+	while ((c = getchar ()) != '6'){
+		if (state == IN && (c == ' ' || c == '\n' || c == '\t'))
+			state = IN;
+		else if (c == ' ' || c == '\n' || c == '\t'){
+			state = IN;
+			putchar('\n');
+		}
+		else {
+			state = OUT;
+			putchar(c);
+		}
+	}
+}
