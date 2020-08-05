64e8c81543d983f24707fdaff20d469f7a67c8be
James Boxall <jimmyboxall@gmail.com>
2020-08-05 12:08:14 +1000

    Finished Exercise 1-13

---
+++ chap1/charCountHist.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/* program to make a histogram of word lengths exercise 1-13 */
+
+void barmaker(int m);
+
+int main() {
+	int c;
+	int nchar = 0;
+	int hist[16];
+	for (int i = 0; i < 16; ++i)
+		hist[i] = 0;
+	
+	while ((c = getchar()) != '6')
+		if (c == ' ' || c == '\n' || c == '\t'){
+			if (nchar > 0)
+				++hist[nchar];
+			nchar = 0;
+		}
+		else
+			++nchar;
+	
+	//print the values in hist
+	printf("\nTable of character lengths of words\n\n");
+	for (int i = 1; i < 16; ++i) {
+		printf("%d " ,i);
+		barmaker(hist[i]);
+	}
+}
+
+void barmaker(int leng)
+{
+       	for (int i = 0; i < leng; ++i)
+		printf("|");
+	printf("\n");
+}
