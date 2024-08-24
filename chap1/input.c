233275e3c3a98779604f6184c9cb7d79b0476fd8
James Boxall <jimmyboxall@gmail.com>
2024-08-24 16:52:50 +1000

    Mucking around with files

--- chap1/input.c
+++ chap1/input.c
@@ -2,8 +2,12 @@
 
 int main()
 {
-	int c;
-	while (c = (getchar()) != EOF)
-		putchar(c);
-	printf("\n");
+	char c;
+    char thing[500];
+    int i=0;
+	while (c = (getchar()) != EOF){
+        thing[i]=c;
+        i++;
+	    printf("%s",thing);
+    }
 }
